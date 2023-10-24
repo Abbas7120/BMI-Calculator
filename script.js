0642bec141b80f84e8834f35dd46b34a6d86bcc7
Syed Mohammad Abbas Rizvi <148750291+Abbas7120@users.noreply.github.com>
2023-10-24 14:03:44 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,25 @@
+function calculateBMI(){
+    var heightInput=document.getElementById("height");
+    var weightInput=document.getElementById("weight");
+    var resultDiv=document.getElementById("result");
+    var height=parseFloat(heightInput.value);
+    var weight=parseFloat(weightInput.value);
+    if(isNaN(height)||isNaN(weight)){
+    resultDiv.innerHTML="Please enter the valid height or weight";return; }
+
+var bmi=weight/((height/100)**2);
+var category="";
+if(bmi<18.5){
+    category="Underweight";
+}
+else if(bmi<25){
+    category="Normal weight";
+}
+else if (bmi < 30) {
+    category = "Overweight";
+  }
+else{
+    category="Obese";
+}
+resultDiv.innerHTML="The BMI of a person is "+bmi.toFixed(2)+" ("+ category + ")";
+}
